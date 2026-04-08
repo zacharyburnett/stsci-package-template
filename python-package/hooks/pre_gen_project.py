194d1ae96ccdfc5f7d7e17cb84592104f395265f
zacharyburnett <zburnett@stsci.edu>
2026-04-08 08:36:33 -0400

    raise error if the user sets hatchling and C extensions

--- python-package/hooks/pre_gen_project.py
+++ python-package/hooks/pre_gen_project.py
@@ -5,8 +5,16 @@ MODULE_REGEX = r"^([_a-zA-Z]+[_a-zA-Z\d]+)$"
 
 PACKAGE_NAME = "{{ cookiecutter.package_name }}"
 
+PYTHON_BUILD_BACKEND = "{{ cookiecutter.python_build_backend }}"
+PYTHON_C_EXTENSIONS = "{{ cookiecutter.python_c_extensions }}" == "True"
+
 if not re.match(MODULE_REGEX, PACKAGE_NAME):
     print(
         f"WARNING: `{PACKAGE_NAME}` may not be importable as a module name in Python",
         file=sys.stderr,
+    )
+
+if PYTHON_BUILD_BACKEND == "hatchling" and PYTHON_C_EXTENSIONS:
+    raise NotImplementedError(
+        "building Python C extensions with Hatch is not yet implemented"
     )
